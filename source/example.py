34c58c0a2ffbd5e59c68029d030ca9e742aa0b23
Nabil Bendafi <nabil@bendafi.fr>
2018-11-04 11:50:53 +0100

    API usage example

---
+++ source/example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+from hw01 import HW01
+
+if __name__ == '__main__':
+    # Rock'n'Roll
+    h = HW01('XX:XX:XX:XX:XX:XX', debug=True)
+    h.setup_services()
+
+    try:
+        battery = h.get_battery_info()
+        print(battery)
+    except KeyboardInterrupt:
+        pass
+
+    try:
+        h.disconnect()
+    except:
+        pass
